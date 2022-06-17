ebc0c7884795d1cba72f5e9c75b7dc4fba1f8530
lekolawole <lekolawole@gmail.com>
2022-06-17 13:45:42 -0400

    Added build

---
+++ client/build/precache-manifest.76c13ff352cfaff57f2ab91fd6539f5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5828711f32cb34784bfb075a9e1a18ec",
+    "url": "/Memry/index.html"
+  },
+  {
+    "revision": "14a3f94790215e660b59",
+    "url": "/Memry/static/css/main.eb88eb82.chunk.css"
+  },
+  {
+    "revision": "08562057e61e9a2753db",
+    "url": "/Memry/static/js/2.6a8798bd.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/Memry/static/js/2.6a8798bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a3f94790215e660b59",
+    "url": "/Memry/static/js/main.9d427066.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Memry/static/js/main.9d427066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b542251bff30cc22192a",
+    "url": "/Memry/static/js/runtime-main.c3145aac.js"
+  },
+  {
+    "revision": "4e20a7b766e1e3cc4a95d54feb181b18",
+    "url": "/Memry/static/media/logo.4e20a7b7.png"
+  },
+  {
+    "revision": "77cb996cd11b15438e24b51cf25c2bde",
+    "url": "/Memry/static/media/text.77cb996c.png"
+  }
+]);
